ce3da44573f5d4b5f03a858d33b6459c80d7befc
G S SRENATH KUMAR <gssrenathkumar2002@gmail.com>
2023-06-25 20:23:55 +0530

    Data Ingestion Implemented Sucessfully

---
+++ main.py
@@ -0,0 +1,14 @@
+from Waste_Recycling_Classifier import logger
+from Waste_Recycling_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
