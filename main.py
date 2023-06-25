78caad6424a99bac109e8fa65cc5eb273aca702e
G S SRENATH KUMAR <gssrenathkumar2002@gmail.com>
2023-06-25 20:52:45 +0530

    Base Model Prepared Sucessfully

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Waste_Recycling_Classifier import logger
 from Waste_Recycling_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Waste_Recycling_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 logger.info("Welcome to our custom logging")
 
@@ -9,6 +10,19 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Preparation of Base Model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>>>>>>>>>>>Stage: {STAGE_NAME} Started Successfully")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>>>>>>Stage: {STAGE_NAME} Completed <<<<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/Waste_Recycling_Classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,20 @@
+from Waste_Recycling_Classifier.config.configuration import ConfigurationManager
+from Waste_Recycling_Classifier.components.prepare_base_model import PrepareBaseModel
+from Waste_Recycling_Classifier import logger
+from Waste_Recycling_Classifier import logger
+
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
